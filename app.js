cf3d55e6ad24e5a40247eef29e9c516809c07a4a
Evandro Favretto <ejfavretto@gmail.com>
2022-05-21 20:26:47 -0300

    Atualização das rotas

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
+const routerCart = require('./routes/routerCart')
+const routerCategory = require('./routes/routerCategory')
+const routerCheckout = require('./routes/routerCheckout')
 const routerHome = require('./routes/routerHome')
+const routerSignInAndOut = require('./routes/routerSignInAndOut')
+const routerUser = require('./routes/routerUser')
 
 const express = require('express')
 const path = require('path')
@@ -9,6 +14,11 @@ app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
+app.use('/', routerCart)
+app.use('/', routerCategory)
+app.use('/', routerCheckout)
 app.use('/', routerHome)
+app.use('/', routerSignInAndOut)
+app.use('/', routerUser)
 
 app.listen(port, console.log(`Servidor rodando na porta ${port}`))
