26bd7918c040bc3f08b972a9b1faa2cc5cb33f62
Evandro Favretto <ejfavretto@gmail.com>
2022-06-04 02:09:16 -0300

    Atualização login, alterar dados usuario e redefinir senha

--- routes/routerUser.js
+++ routes/routerUser.js
@@ -3,6 +3,21 @@ const controllerUser = require('../controllers/controllerUser')
 const express = require('express')
 const router = express.Router()
 
-router.get('/user', controllerUser.index)
+// router.get('/user', controllerUser.index)
+
+router.get('/user/createUser', controllerUser.createUser)
+router.post('/user/storeUser', controllerUser.storeUser)
+
+router.get('/user/editUser', controllerUser.editUser)
+router.post('/user/updateUser', controllerUser.updateUser)
+
+router.get('/user/editPassword', controllerUser.editPassword)
+router.post('/user/updatePassword', controllerUser.updatePassword)
+
+router.get('/user/userSignIn', controllerUser.userSignIn)
+router.post('/user/userLogged', controllerUser.userLogged)
+
+router.get('/user/userSignUp', controllerUser.userSignUp)
+router.post('/user/userCreated', controllerUser.userCreated)
 
 module.exports = router
